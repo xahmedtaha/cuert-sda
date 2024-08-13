86ee31a447fe8033efab0d92c0c8b9f394a205e7
Ahmed Taha <xahmedtaha930@gmail.com>
2024-08-13 22:24:33 +0300

    Force HTTPS at production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         JsonResource::withoutWrapping();
+        if (app()->isProduction()) URL::forceScheme('https');
     }
 }
